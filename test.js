3b8c90930a64af0758e0a2ab5a0ed046d2126384
Miguel Calles <mcalles@veritoll.com>
2020-07-08 15:44:33 -0700

    Added new tests

--- test.js
+++ test.js
@@ -121,5 +121,49 @@ describe('plugin', () => {
                 EXPAND_SHELL: 'basic',
             });
         });
+        test('expand first with dependencies', () => {
+            process.env.STAGE = 'dep';
+            serverless.service.custom = {
+                setenv: {
+                    dotenv: {
+                        expand: true,
+                        shell: true,
+                        order: ['expand', 'shell'],
+                    },
+                },
+            };
+            new ServerlessPlugin(serverless);
+            expect(process.env).toEqual({
+                STAGE: 'dep',
+                BASIC: 'basic',
+                EXPAND: 'basic',
+                SHELL: 'basic',
+                EXPAND_SHELL: 'basic',
+                SHELL_DEP1: 'shell1',
+                SHELL_DEP1A: 'shell1',
+            });
+        });
+        test('shell first with dependencies', () => {
+            process.env.STAGE = 'dep';
+            serverless.service.custom = {
+                setenv: {
+                    dotenv: {
+                        expand: true,
+                        shell: true,
+                        order: ['shell', 'expand'],
+                    },
+                },
+            };
+            new ServerlessPlugin(serverless);
+            expect(process.env).toEqual({
+                STAGE: 'dep',
+                BASIC: 'basic',
+                EXPAND: 'basic',
+                SHELL: 'basic',
+                EXPAND_SHELL: 'basic',
+                SHELL_DEP1: 'shell1',
+                SHELL_DEP1A: 'shell1',
+            });
+        });
     });
 });
